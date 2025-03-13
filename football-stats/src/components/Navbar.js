8d4022383fb7c733e38536eef91c9ecca353566f
FrancoisleRoux <159991396+231256leRouxFNF@users.noreply.github.com>
2025-03-13 08:55:57 +0200

    done
    done for progress check one

--- football-stats/src/components/Navbar.js
+++ football-stats/src/components/Navbar.js
@@ -3,11 +3,14 @@ import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Navbar, Nav, Button } from 'react-bootstrap';
 import './Navbar.css'; // Import the CSS file
+import logo from '../imgs/Logo.svg'; // Import the logo image with the correct case
 
 const CustomNavbar = () => {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" className="flex-column vh-100">
-      <Navbar.Brand href="/" className="mb-4">⚽ Football Stats</Navbar.Brand>
+      <Navbar.Brand href="/" className="mb-4">
+        <img src={logo} alt="Football Stats Logo" style={{ width: '120px', height: '120px' }} />
+      </Navbar.Brand>
       <Nav className="flex-column w-100">
         <Nav.Link as={Link} to="/" className="nav-link">Home</Nav.Link>
         <Nav.Link as={Link} to="/compare" className="nav-link">Compare</Nav.Link>
